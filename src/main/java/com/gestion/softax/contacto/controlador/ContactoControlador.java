fd7ac4b890b1bc09e4f91fc41eaa7807f2afe91b
Alfonsoftax <alfonsoftax@gmail.com>
2022-08-22 20:16:05 +0200

    cambio

--- src/main/java/com/gestion/softax/contacto/controlador/ContactoControlador.java
+++ src/main/java/com/gestion/softax/contacto/controlador/ContactoControlador.java
@@ -24,7 +24,7 @@ import com.gestion.softax.contacto.repositorio.ContactoRepositorio;
 @RestController
 @RequestMapping("/api/v1/")
 //@CrossOrigin(origins = "http://localhost:4200/")
-@CrossOrigin(origins = "https://softax.es/")
+@CrossOrigin(origins = "http://softax.es/")
 public class ContactoControlador {
 
 	@Autowired
